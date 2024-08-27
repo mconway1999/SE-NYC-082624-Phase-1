39ab43f8592108cea8ae051d6d4a6d5534ef537f
Madeline Conway <conway.maddy@yahoo.com>
2024-08-27 10:08:40 -0400

    completed

---
+++ src/index.js
@@ -0,0 +1,55 @@
+//console.log("hello")
+
+//alert('hello')
+//const personName = prompt ("please enter your name")
+//alert welcome to flatburger, $(person name)
+
+
+//const num1 = prompt ("enter your first number")
+//const num2 = Number ("enter your second number")
+//console.log(num2)
+//console.log(typeof num2)
+//alert ('the sum of of ${num1} + ${num2} is ${num1 + num2}')
+
+//console.log(7 === 7)
+//console.log(7 !== 7)
+//console.log (7 == 7)
+//debugger examples
+//let personName = "Alice"
+//let age = 21
+//console.log (personName)
+//console.log (age)
+
+//const personName = "Alice"
+//const age = 21
+//const isStudent = true
+
+//let greetingPhrase
+
+//if(isStudent === true){
+//    greetingPhrase = "Hello ${personName}! welcome to flatiron school!"
+//}
+//else{
+//    greetingPhrase = "goodbye!"
+//}
+
+//console.log (greetingPhrase)
+
+//ternery operator
+//let greetingPhrase = isStudent === true ? 'Hello ${personName}! Welcome to flatiron school' : "Goodbye!"
+
+//Switch statements
+
+//let greetingPhrase
+
+//switch (isStudent === true) {
+  //  case true:
+    //    greetingPhrase = 'Hello ${personName}! Welcome to flatiron school'
+      //  console.log(isStudent)
+        //break
+    //default:
+      //  greetingPhrase = "Goodbye!"
+//}
+//console.log(greetingPhrase)
+
+
